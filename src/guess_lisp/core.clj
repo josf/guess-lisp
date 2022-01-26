06e79e9560909d2466fc0336c8a007728ab3f267
Joseph Fahey <fahey.joseph@gmail.com>
2022-01-26 08:57:11 +0100

    An educated guess for the next word (letter freq strategy)

--- src/guess_lisp/core.clj
+++ src/guess_lisp/core.clj
@@ -103,3 +103,103 @@
     z)
    ;; => ("robot" "rotor")
    )
+
+
+(defn letter-frequencies-by-position
+  [words position]
+  (frequencies (map #(nth % position) words)))
+
+(defn- deduped-word-score
+  [word freqs]
+  (apply +
+         (vals
+           (reduce
+             (fn [acc [letter fqs]]
+               (assoc acc letter (get fqs letter)))
+             {}
+             (map vector word freqs)))))
+
+(defn ranked-guesses
+  [words]
+  (let [freqs (map (partial letter-frequencies-by-position words) (range 5))]
+    (->> words
+         (map
+           (fn [word]
+             {:word word
+              :score 
+              (deduped-word-score word freqs)}))
+         (sort-by :score))))
+
+
+(defn simulate
+  [target-word words matches guess-count]
+  (let [filtered-words (keep (omni-pred (omni-pred-data matches)) words)
+        ranked (ranked-guesses filtered-words)]
+    (cond (> guess-count 6)
+          {:failed-after 6 :word target-word :matches matches}
+
+          (= 1 (count ranked))
+          {:found (:word (first ranked)) :guesses guess-count}
+
+          :else
+          (recur target-word filtered-words
+                 (conj matches (compare-words target-word (:word (last ranked)))) (inc guess-count)))))
+
+
+(defn word-response
+  "Convenience function for manually entering a real Wordle from the website.
+  x = correct letter (`:correct`),
+  ! = wrong letter (`:not-in-word`),
+  ? = correct letter but wrong position. (`:in-word`)"
+  [word wordle-response]
+  (when (not= 5 (count wordle-response))
+    (throw (ex-info "Incorrect wordle reponse length"  {:length (count wordle-response)})))
+  (map (fn [letter code]
+         (case code
+           \x {:correct letter}
+           \! {:not-in-word letter}
+           \? {:in-word letter}))
+       word wordle-response))
+
+
+(comment
+
+  (best-guess (keep (omni-pred
+                      (omni-pred-data
+                      [[{:not-in-word \s}
+                        {:in-word \o}
+                        {:not-in-word \r}
+                        {:not-in-word \e}
+                        {:not-in-word \s}]
+                       [{:not-in-word \a}
+                        {:in-word \l}
+                        {:correct \o}
+                        {:not-in-word \o}
+                        {:not-in-word \f}]
+                       [{:not-in-word \g}
+                        {:not-in-word \h}
+                        {:correct \o}
+                        {:not-in-word \u}
+                        {:correct \l}]])) z))
+
+
+(best-guess (keep (omni-pred
+                    (omni-pred-data
+                      [[{:correct \s}
+                        {:not-in-word \o}
+                        {:in-word \r}
+                        {:not-in-word \e}
+                        {:not-in-word \s}]
+                       [{:correct \s}
+                        {:not-in-word \t}
+                        {:in-word \a}
+                        {:in-word \r}
+                        {:not-in-word \s}]]))
+                  z))
+
+
+(best-guess (keep (omni-pred
+                    (omni-pred-data
+                      [(word-response "cares" "??!!!")
+                       (word-response "alack" "!!xxx")]))
+                  z)))
